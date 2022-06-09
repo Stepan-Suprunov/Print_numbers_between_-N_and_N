884caa2508e0a2bacbcae931e009893a50a806d0
Stepan_Suprunov <ipsuprunovso@mail.ru>
2022-06-09 20:06:58 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// Выводит числа в промежутке от -N до -N
+Console.WriteLine("Задайте число N");
+int Nmax = int.Parse(Console.ReadLine());
+int Nmin = - Nmax;
+while (Nmin <= Nmax) {
+    Console.Write(Nmin + ",");
+    Nmin ++;
+}
